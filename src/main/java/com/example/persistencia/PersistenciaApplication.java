61ef95eb067f13cf56bb6ef02ed97568d852dea7
Cristian Serrano <cristiangserrano@gmail.com>
2021-12-22 16:21:05 -0500

    Compilacion

--- src/main/java/com/example/persistencia/PersistenciaApplication.java
+++ src/main/java/com/example/persistencia/PersistenciaApplication.java
@@ -5,11 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import main.java.Backend.GetMapping;
-import main.java.Backend.Personas;
 
 @SpringBootApplication
 @RestController
